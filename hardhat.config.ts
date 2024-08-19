432af97b3d142c1f939f6521cebda54386204def
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-08-19 21:02:25 +0300

    Solidity version 0.8.26, evmVersion shanghai

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.26",
+    settings: {
+      evmVersion: "shanghai",
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        details: {
+          yulDetails: {
+            optimizerSteps: "u",
+          },
+        },
+      },
+    },
+  },
 };
 
 export default config;
